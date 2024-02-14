d60c517adf7c1491257a51aea2bcb288cd48af1d
prashantsingh15 <prashantsingh15092002@gmail.com>
2024-02-14 06:14:41 +0530

    Commit

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from PIL import Image
 
-
 def load_model_and_vectorizer(model_path, vectorizer_path):
     with open(model_path, 'rb') as model_file:
         loaded_model = pickle.load(model_file)
@@ -14,26 +13,19 @@ def load_model_and_vectorizer(model_path, vectorizer_path):
     
     return loaded_model, vectorizer
 
-
 def preprocess_and_vectorize(text, vectorizer):
-    
     vectorized_text = vectorizer.transform([text])
     return vectorized_text
 
-
-def predict_sentiment(model, vectorized_text):
-    
+def predict_disaster(model, vectorized_text):
     prediction = model.predict(vectorized_text)[0]
     return prediction
 
-
 def main():
-   
-    model_path = 'sentiment_model.pkl'
+    model_path = 'disaster_model.pkl'
     vectorizer_path = 'tfidf_vectorizer.pkl'
     loaded_model, vectorizer = load_model_and_vectorizer(model_path, vectorizer_path)
 
-    
     st.markdown(
         """
         <style>
@@ -45,28 +37,19 @@ def main():
         unsafe_allow_html=True
     )
 
-   
-    st.title('Twitter Sentiment Analysis')
-    st.write("This app analyzes the sentiment of a tweet and predicts whether it's positive, neutral, or negative.")
+    st.title('Twitter Disaster Detection')
+    st.write("This app analyzes a tweet and predicts whether it's about a disaster or a normal situation.")
 
-   
     user_input = st.text_area('Enter a tweet:')
     
-    if st.button('Analyze Sentiment'):
+    if st.button('Analyze Tweet'):
         if user_input:
-            
             vectorized_text = preprocess_and_vectorize(user_input, vectorizer)
+            prediction = predict_disaster(loaded_model, vectorized_text)
+            disaster_mapping = {1: 'Disaster 🚨', 0: 'Normal 🌤️'}
+            disaster_result = disaster_mapping.get(prediction, 'Unknown')
+            st.write(f"Tweet Type: {disaster_result}")
 
-            
-            prediction = predict_sentiment(loaded_model, vectorized_text)
-
-            
-            sentiment_mapping = {1: 'Positive 😄', 0: 'Neutral 😐', -1: 'Negative 😞'}
-            sentiment_result = sentiment_mapping.get(prediction, 'Unknown')
-            
-            st.write(f"Sentiment: {sentiment_result}")
-
-   
     image_path = 'background.png'  
     image = Image.open(image_path)
     st.image(image, use_column_width=True)
